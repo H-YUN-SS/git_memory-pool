939a4bde0c29c181bd9cb1d16cac3f804a2355a5
H-YUN-SS <920563885@qq.com>
2026-04-03 16:01:10 +0800

    feat: implement lock-free memory pool with allocation & deallocation

--- v1/src/MemoryPool.cpp
+++ v1/src/MemoryPool.cpp
@@ -14,26 +14,75 @@ namespace Kama_memoryPool
     }
     MemoryPool::~MemoryPool()
     {
-
+        Slot* cur=firstBlock_;
+        while(cur!=nullptr)
+        {
+            Slot* next=cur->next;
+            operator delete(reinterpret_cast<void*>(cur));
+            cur=next;
+        }
     }
     
     void* MemoryPool::allocate()
     {
-        return nullptr;
+        Slot* slot=popFreeList();
+        if(slot!=nullptr)
+        {
+            return slot;
+        }
+        Slot* temp;
+        {
+            std::lock_guard<std::mutex>lock(mutexForBlock_);
+            if(curSlot_>=lastSlot_)
+            {
+                allocateNewBlock();
+            }
+        
+        temp=curSlot_;
+        curSlot_+=SlotSize_/sizeof(slot);//curslot_&&slot都是slot*类型
+        }
+        return temp;
     }
 
     void MemoryPool::deallocate(void* ptr)
     {
+        if(!ptr)//空指针
+        {
+            return;
+        }
+        Slot* slot=reinterpret_cast<Slot*>(ptr);
+        pushFreeList(slot);
 
     }
     
     bool MemoryPool::pushFreeList(Slot*slot)
     {
-        return false;
+        while(true)
+        {
+            Slot* oldHead=freeList_.load(std::memory_order_relaxed);
+            slot->next.store(oldHead, std::memory_order_relaxed);
+            if(freeList_.compare_exchange_weak(oldHead,slot))
+            {
+                return true;
+            }
+        }
     }
 
     Slot* MemoryPool::popFreeList()
     {
-        return nullptr;
+        while(true)
+        {
+            Slot* oldHead=freeList_.load(std::memory_order_acquire);
+            if(oldHead==nullptr)
+            {
+                return nullptr;
+            }
+            Slot* newHead=oldHead->next.load(std::memory_order_relaxed);
+            
+            if(freeList_.compare_exchange_weak(oldHead,newHead))
+            {
+                return oldHead;
+            }
+        }
     }
 }
