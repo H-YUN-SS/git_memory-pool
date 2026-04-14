3a00a432bed14136c930d0db170b7dfdfe1b653b
H-YUN-SS <920563885@qq.com>
2026-04-14 20:54:42 +0800

    feat: add ThreadCache.h (thread-local cache declaration)

---
+++ v2/include/ThreadCache.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Common.h"
+
+namespace Kama_memoryPool
+{
+
+
+
+    // 线程局部单例模式
+    // static thread_local：每个线程创建自己独立的 instance
+    // 作用：保证每个线程只有一个 ThreadCache
+    class ThreadCache
+    {
+        public:
+        static ThreadCache* getInstance()
+        {
+            static thread_local ThreadCache instance;
+            return &instance;
+        }
+    }
+}
